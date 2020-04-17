0606cc8cb9f467e9f22fdbd18fa2b54c8e700545
王鹏 <924613197@qq.com>
2020-04-17 17:53:52 +0800

    [修改]路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ Vue.use(Router)
 
 function route(path, file, name, children) {
   return {
-    exact: true,
+    // exact: true,
     path,
     name,
     children,
@@ -13,26 +13,31 @@ function route(path, file, name, children) {
   }
 }
 
-const router =  new Router({
+const router = new Router({
   routes: [
     route("/login", '/Login', "Login"),
     route("/register", '/Register', "Register"),
     {
-      path: '/',
-      component: () => import('../pages/Home'),
-      redirect: '/index/dashboard',
+      path: '/admin',
+      component: () => import('../pages/admin/Home'),
+      redirect: '/admin/index/dashboard',
       children: [
-        route("/index/dashboard", "/Dashboard", "Dashboard"),
-        route("/test1/test1-1", '/test1/test1-1', "test1-1"),
-        route("/test1/test1-2", '/test1/test1-2', "test1-2"),
-        route("/test1/test1-3", '/test1/test1-3', "test1-3"),
-        route("/test2/test2-1", '/test2/test2-1', "test2-1"),
-        route("/test2/test2-2", '/test2/test2-2', "test2-2"),
-        route("/authority/list", '/authority/list', "List"),
-        route("/authority/member", '/authority/member', "member"),
-        route("/authority/role", '/authority/role', "role")
+        route("index/dashboard", "/admin/Dashboard", "Dashboard"),
+        route("test1/test1-1", '/admin/test1/test1-1', "test1-1"),
+        route("test1/test1-2", '/admin/test1/test1-2', "test1-2"),
+        route("test1/test1-3", '/admin/test1/test1-3', "test1-3"),
+        route("test2/test2-1", '/admin/test2/test2-1', "test2-1"),
+        route("test2/test2-2", '/admin/test2/test2-2', "test2-2"),
+        route("authority/list", '/admin/authority/list', "List"),
+        route("authority/member", '/admin/authority/member', "member"),
+        route("authority/role", '/admin/authority/role', "role")
       ]
-    }
+    },
+    route("/index", "/index", "index"),
+    {
+      path: '/',
+      redirect: '/index',
+    },
   ]
 })
 
