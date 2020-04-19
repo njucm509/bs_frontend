788052ee1d602d33b63f781cf5e6c4f726943aa8
王鹏 <njucmwp@126.com>
2020-04-19 23:22:21 +0800

    [新增]新增登陆界面

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,65 @@
 <template>
+  <div id="login">
+    <el-form :model="login" :rules="login_rule" ref="login" class="login" style="width: 300px;">
+      <h3 align="center" style="padding-bottom: 20px;padding-top: 20px;">登陆</h3>
+      <el-form-item prop="username">
+        <el-input v-model="login.name" placeholder="请输入账号..." style=""></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="login.password" placeholder="请输入密码..." show-password></el-input>
+      </el-form-item>
+      <el-form-item style="text-align: center">
+        <el-button @click="submit('login')" type="primary" style="width:100%">登陆</el-button>
+        <router-link to="/register">
+          <a type="primary">注册</a>
+        </router-link>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        login: {
+          name: '',
+          password: ''
+        },
+        login_rule: {
+          name: [{
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }],
+          password: [{
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          }]
+        }
+      }
+    },
+    methods: {
+      submit(form) {
+        this.$refs[form].validate((valid) => {
+          if (valid) {
 
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped>
+  .login {
+    border-radius: 5px;
+    background-clip: padding-box;
+    width: 350px;
+    margin: 180px auto;
+    background-color: #FFFFFF;
+    padding: 35px 35px 15px 35px;
+    box-shadow: 0px 0px 25px #cac6c6;
+  }
 </style>
